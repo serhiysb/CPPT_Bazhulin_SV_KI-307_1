6aabd2236ca943e805af7f9dd739444a4c049eec
serhiy <bserhiy48@gmail.com>
2023-09-23 14:01:14 +0300

    Update Lab3

---
+++ LAB_03/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"KI307.Bazhulin.Lab3"}]
---
+++ LAB_03/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"KI307.Bazhulin.Lab3","l":"ReusableSpaceship"},{"p":"KI307.Bazhulin.Lab3","l":"Spaceship"},{"p":"KI307.Bazhulin.Lab3","l":"SpaceshipApp"}]
